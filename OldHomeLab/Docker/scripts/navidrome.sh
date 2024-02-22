424cf1e434a2483b5db77bd739a3174ce04acd64
James Young <jyyoungdev@proton.me>
2024-02-22 13:21:01 +0800

    remove archive

--- OldHomeLab/Docker/scripts/navidrome.sh
+++
@@ -1,9 +0,0 @@
-docker run -d \
-   --name navidrome \
-   --restart=unless-stopped \
-   --user $(id -u):$(id -g) \
-   -v /new-pool/jellyfin/media/music:/music \
-   -v /new-pool/jellyfin/media/navidrome-data:/data \
-   -p 4533:4533 \
-   -e ND_LOGLEVEL=info \
-   deluan/navidrome:latest
